3231d274b5dbeef51266af6750b9db7dee287cc8
wenyuan <wenyuan151@gmail.com>
2016-07-23 17:50:41 +0800

    this project is for learn java

---
+++ src/main/java/annotation/FruitRun.java
@@ -0,0 +1,14 @@
+package annotation;
+
+public class FruitRun {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        FruitInfoUtil.getFruitInfo(Apple.class);
+        
+    }
+
+}
